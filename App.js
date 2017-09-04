cee986814b5d6642da776de49470c95b2a14d851
Jerel Miller <jerelmiller@gmail.com>
2017-09-03 23:25:23 -0700

    Set app header color and background

--- App.js
+++ App.js
@@ -3,12 +3,21 @@ import { StyleSheet, Text, View } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 import PokemonList from './screens/PokemonList'
 import withApollo from './components/withApollo'
+import themes from './lib/themes'
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: themes.app.primary
+  }
+})
 
 const App = StackNavigator({
   Home: {
     screen: withApollo(PokemonList),
     navigationOptions: {
-      title: 'Pokedex'
+      title: 'Pokedex',
+      headerStyle: styles.header,
+      headerTintColor: themes.app.text
     }
   }
 })
