49f64c0f120acccde5cfc0eadecf4c4f95076f41
Jerel Miller <jerelmiller@gmail.com>
2017-09-03 22:19:35 -0700

    Add the apollo client

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { ApolloClient, ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient()
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ApolloProvider client={ client }>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </ApolloProvider>
     )
   }
 }
