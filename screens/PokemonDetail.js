a121c61d866eb686e1591b8b9ac2974c4b5d84f2
Jerel Miller <jerelmiller@gmail.com>
2017-09-04 01:34:11 -0700

    Add basic detail screen

---
+++ screens/PokemonDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PokemonDetail = () => (
+  <View>
+    <Text>Pokemon</Text>
+  </View>
+)
+
+export default PokemonDetail
