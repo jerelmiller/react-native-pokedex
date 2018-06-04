9b7874c7a08311a9ffd33ebf59cdedbe0e0e85d1
Jerel Miller <jerelmiller@gmail.com>
2018-06-03 22:26:12 -0700

    Add fragment for header to load when navigating to a pokemon

--- screens/PokemonDetail.js
+++ screens/PokemonDetail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import gql from 'graphql-tag'
 import { View, Text } from 'react-native'
 
 const PokemonDetail = () => (
@@ -6,5 +7,15 @@ const PokemonDetail = () => (
     <Text>Pokemon</Text>
   </View>
 )
+
+PokemonDetail.fragments = {
+  pokemonHeader: gql`
+    fragment PokemonHeader_pokemon on Pokemon {
+      name
+      number
+      types
+    }
+  `
+}
 
 export default PokemonDetail
