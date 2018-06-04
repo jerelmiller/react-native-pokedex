55c04f0229490c4fe062c62b7d0ed5486a304a8a
Jerel Miller <jerelmiller@gmail.com>
2018-06-03 21:57:18 -0700

    Update pokemon list to use query component

--- screens/PokemonList.js
+++ screens/PokemonList.js
@@ -2,27 +2,41 @@ import React from 'react'
 import { ActivityIndicator, StyleSheet, ScrollView, View } from 'react-native'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
-import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo'
 import PokemonCard from '../components/PokemonCard'
 
 const Container = styled.View`
   flex: 1;
 `
 
-const PokemonList = ({ data: { loading, pokemons = [] }, navigation }) => (
-  <Container>
-    {loading && <ActivityIndicator animating={loading} size="large" />}
-    <ScrollView contentContainerStyle={styles.scrollView}>
-      {pokemons.map(pokemon => (
-        <PokemonCard
-          key={pokemon.id}
-          pokemon={pokemon}
-          onPress={() => navigation.navigate('Detail', { pokemon })}
-          style={{ width: '50%' }}
-        />
-      ))}
-    </ScrollView>
-  </Container>
+const PokemonList = ({ navigation }) => (
+  <Query
+    query={gql`
+      query {
+        pokemons {
+          id
+          ...PokemonCard
+        }
+      }
+      ${PokemonCard.fragments.pokemon}
+    `}
+  >
+    {({ loading, data: { pokemons } }) => (
+      <Container>
+        {loading && <ActivityIndicator animating={loading} size="large" />}
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          {pokemons.map(pokemon => (
+            <PokemonCard
+              key={pokemon.id}
+              pokemon={pokemon}
+              onPress={() => navigation.navigate('Detail', { pokemon })}
+              style={{ width: '50%' }}
+            />
+          ))}
+        </ScrollView>
+      </Container>
+    )}
+  </Query>
 )
 
 const styles = StyleSheet.create({
@@ -32,12 +46,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default graphql(gql`
-  query {
-    pokemons {
-      id
-      ...PokemonCard
-    }
-  }
-  ${PokemonCard.fragments.pokemon}
-`)(PokemonList)
+export default PokemonList
